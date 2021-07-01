c20e84fa3a3001c0dc4685e087f7833121f95aac
Mostafa El-Sayed <73605018+jaycelol10@users.noreply.github.com>
2021-07-01 23:35:16 +0200

    Add files via upload
    MVC -Website

---
+++ datamod.php
@@ -0,0 +1,5 @@
+<?php
+require_once  'controller/sportsController.php';		
+    $controller = new sportsController();	
+    $controller->mvcHandler();
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	session_unset();
+	include("model/MLogin.php");
+    include("view/VLogin.php");
+    
+    	
+    
+   
+	?>
